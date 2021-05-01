37139af9c7a74d84cbc4ff9d4c429cf2930a4b9a
Irene <igarzona@estudiantec.cr>
2021-04-30 20:04:55 -0600

    ,

--- src/index.js
+++ src/index.js
@@ -10,7 +10,12 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+let baseUrl='http https://swapi.dev/api/people/1/'
 
+fetch(baseUrl).then(response => response.json())
+.then(json =>{
+
+})
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
